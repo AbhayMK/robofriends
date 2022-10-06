7177c107e31f0dd7b5bdf8c6095030d36574c6eb
AbhayMK <abhay.physics@gmail.com>
2022-10-06 22:09:38 +0530

    Updates

---
+++ precache-manifest.ec7f434c2c95844ecebb3ed960cd3dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d1a29476a122085e46e",
+    "url": "/robofriends/static/css/main.b692fff2.chunk.css"
+  },
+  {
+    "revision": "8d1a29476a122085e46e",
+    "url": "/robofriends/static/js/main.cbb34167.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "08c533e78ae2a7232bc9",
+    "url": "/robofriends/static/css/2.87333c09.chunk.css"
+  },
+  {
+    "revision": "08c533e78ae2a7232bc9",
+    "url": "/robofriends/static/js/2.cf8dcc85.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "eafb30f2cf318c6ae20200263005ec52",
+    "url": "/robofriends/index.html"
+  }
+];
